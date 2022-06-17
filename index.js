350f7dce8c70bdac6a4674972c89503eecee5647
Jon Eastin <picardncce@gmail.com>
2022-06-17 10:40:32 -0700

    refactored all sort methods and added the necessary html to run them

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('Loaded')
-})
-
 const descriptions = document.querySelectorAll('.description-display'),
   ratings = document.querySelectorAll('.rating-display > .value'),
   parks = document.querySelectorAll('.park-display'),
@@ -15,24 +11,24 @@ newElement.innerText = `${numberParks} exciting parks to visit`
 newElement.classList.add('header-statement')
 header.appendChild(newElement)
 
-for (let desc of descriptions.values()) {
-  let content = desc.innerText
-  if (content.length > 250) {
-    content = content.slice(0, 250)
-    content = content + '...'
-  }
+// for (let desc of descriptions.values()) {
+//   let content = desc.innerText
+//   if (content.length > 250) {
+//     content = content.slice(0, 250)
+//     content = content + '...'
+//   }
 
-  console.log(content)
-}
+//   console.log(content)
+// }
 
-for (let rating of ratings) {
-  let ratingValue = parseFloat(rating.innerText)
+// for (let rating of ratings) {
+//   let ratingValue = parseFloat(rating.innerText)
 
-  if (ratingValue > 4.7) {
-    rating.classList.add('high-rating')
-    rating.classList.remove('value')
-  }
-}
+//   if (ratingValue > 4.7) {
+//     rating.classList.add('high-rating')
+//     rating.classList.remove('value')
+//   }
+// }
 
 allBtns.forEach((btn) => {
   btn.addEventListener('click', (event) => {
@@ -42,17 +38,27 @@ allBtns.forEach((btn) => {
 })
 
 const sortByName = (parkA, parkB) => {
-  parksArray.sort((parkA, parkB) => {
-    const parkAName = parkA.querySelector('h2').innerText
-    const parkBName = parkB.querySelector('h2').innerText
-    if (parkAName < parkBName) {
-      return -1
-    } else if (parkAName > parkBName) {
-      return 1
-    } else {
-      return 0
-    }
-  })
+  const parkAName = parkA.querySelector('h2').innerText
+  const parkBName = parkB.querySelector('h2').innerText
+  if (parkAName < parkBName) {
+    return -1
+  } else if (parkAName > parkBName) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+const sortByRating = (parkA, parkB) => {
+  const parkARating = parkA.querySelector('.stats > .rating-display > .value').innerText
+  const parkBRating = parkB.querySelector('.stats > .rating-display > .value').innerText
+  if (parkARating < parkBRating) {
+    return -1
+  } else if (parkARating > parkBRating) {
+    return 1
+  } else {
+    return 0
+  }
 }
 
 const nameSorterClickHandler = (event) => {
@@ -73,6 +79,31 @@ const nameSorterClickHandler = (event) => {
   })
 }
 
-const nameSorter = document.querySelector('#name-sorter');
+const rateSorterClickHander = (event) => {
+  event.preventDefault()
 
-nameSorter.addEventListener('click', nameSorterClickHandler)
+  const main = document.querySelector('main')
+
+  const parksList = main.querySelectorAll('.park-display')
+
+  main.innerHTML = ''
+
+  parksArray = Array.from(parksList)
+
+  parksArray.sort(sortByRating)
+
+  parksArray.forEach((park) => {
+    main.appendChild(park)
+  })
+}
+
+const main = () => {
+  const nameSorter = document.querySelector('#name-sorter')
+
+  nameSorter.addEventListener('click', nameSorterClickHandler)
+
+  const rateSorter = document.querySelector('#rating-sorter')
+  rateSorter.addEventListener('click', rateSorterClickHander)
+}
+
+window.addEventListener('DOMContentLoaded', main)
